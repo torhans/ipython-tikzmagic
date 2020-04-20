36f72f344cdf25ee40a304c9e39cb64be1120720
Torben Hansen <tmh@localhost.localdomain>
2020-04-20 18:00:57 +0200

    testing differnt conversion methods

--- tikzmagic.py
+++ tikzmagic.py
@@ -31,7 +31,7 @@ import sys
 import tempfile
 from glob import glob
 from os import chdir, getcwd, environ, pathsep
-from subprocess import call
+from subprocess import call,Popen,PIPE
 from shutil import rmtree, copy
 from xml.dom import minidom
 
@@ -43,6 +43,12 @@ from IPython.core.magic_arguments import (
     argument, magic_arguments, parse_argstring
 )
 from IPython.utils.py3compat import unicode_to_str
+
+# defaults
+default_convert='svg'
+default_height=240
+default_width=400
+default_resolution=300
 
 try:
     import pkg_resources  # part of setuptools
@@ -90,7 +96,7 @@ class TikzMagics(Magics):
 
         """
         (svg,) = minidom.parseString(image).getElementsByTagName('svg')
-        viewbox = svg.getAttribute('viewBox').split(' ')
+        viewbox = [float(s) for s in svg.getAttribute('viewBox').split(' ')]
 
         if size is not None:
             width, height = size
@@ -101,8 +107,28 @@ class TikzMagics(Magics):
         svg.setAttribute('height', '%dpx' % height)
         return svg.toxml()
 
+    def _fix_gnuplot_svg_file(self, dir):
+        (svg,) = minidom.parse(dir+'/tikz.svg').getElementsByTagName('svg')
+        viewbox = [float(s) for s in svg.getAttribute('viewBox').split(' ')]
 
-    def _run_latex(self, code, encoding, dir):
+        width, height = viewbox[2:]
+
+        svg.setAttribute('width', '%dpx' % width)
+        svg.setAttribute('height', '%dpx' % height)
+        with open(dir+'/tikz.svg','w') as f:
+            svg.writexml(f)
+        return width,height
+
+    
+    def _get_size_from_svg(self,dir):
+        (svg,) = minidom.parse(dir+'/tikz.svg').getElementsByTagName('svg')
+        w=float(svg.getAttribute('width')[:-2])
+        h=float(svg.getAttribute('height')[:-2])
+        return(w,h)
+        
+
+
+    def _run_latex(self, code, encoding, dir, exe="pdflatex"):
         f = open(dir + '/tikz.tex', 'w', encoding=encoding)
         f.write(code)
         f.close()
@@ -124,7 +150,8 @@ class TikzMagics(Magics):
             # search path (otherwise we would lose access to all packages)
 
         try:
-            retcode = call("pdflatex --shell-escape tikz.tex", shell=True, env=env)
+            retcode = call(exe+" --shell-escape tikz.tex", shell=True, env=env)
+            #retcode = call("pdflatex --shell-escape tikz.tex", shell=True, env=env)
             if retcode != 0:
                 print("LaTeX terminated with signal", -retcode, file=sys.stderr)
                 ret_log = True
@@ -145,6 +172,34 @@ class TikzMagics(Magics):
 
         return log
 
+    def _convert_dvi_to_svg(self, dir):
+        current_dir = getcwd()
+        chdir(dir)
+
+        try:
+            dvip=Popen(['dvisvgm','tikz'],stdout=PIPE,stderr=PIPE)
+            out,err=dvip.communicate()
+            if err[:6]==b'ERROR:':
+                print("dvisvgm terminated with signal", err.decode('utf-8'), file=sys.stderr)
+        except OSError as e:
+            print("dvisvgm execution failed:", e, file=sys.stderr)
+
+        chdir(current_dir)
+
+    def _shell(self, dir,cmd,exarg=None):
+        current_dir = getcwd()
+        chdir(dir)
+        try:
+            if exarg is not None:
+                proc=Popen([cmd,exarg],stdout=PIPE,stderr=PIPE)
+            else:
+                proc=Popen([cmd],stdout=PIPE,stderr=PIPE)
+            out,err=proc.communicate()
+            print(out.decode('utf-8'),err.decode('utf-8'))
+        except OSError as e:
+            print("shell execution failed:", e, file=sys.stderr)
+
+        chdir(current_dir)
 
     def _convert_pdf_to_svg(self, dir):
         current_dir = getcwd()
@@ -159,6 +214,32 @@ class TikzMagics(Magics):
 
         chdir(current_dir)
 
+
+    def _convert_svg_to_raster(self, dir, imagemagick_path,resolution,resolution_height=None,width=None,height=None,plot_format='png'):
+        current_dir = getcwd()
+        chdir(dir)
+        
+        callstr='{} -quality 100 -background white -flatten -density {}'.format(imagemagick_path,resolution)
+        if resolution_height is not None:
+            callstr+='x{}'.format(resolution_height)
+        if width is not None:
+            callstr+=' -extent {}'.format(width)
+        if height is not None:
+            callstr+='x{}'.format(height)
+        callstr+=' tikz.svg tikz.{}'.format(plot_format)
+        
+        print(callstr)
+        
+        call("convert", shell=False)
+
+        try:
+            retcode = call(callstr, shell=True)
+            if retcode != 0:
+                print("convert terminated with signal", -retcode, file=sys.stderr)
+        except OSError as e:
+            print("convert execution failed:", e, file=sys.stderr)
+
+        chdir(current_dir)
 
     def _convert_png_to_jpg(self, dir, imagemagick_path):
         current_dir = getcwd()
@@ -182,7 +263,7 @@ class TikzMagics(Magics):
         help='Scaling factor of plots. Default is "--scale 1".'
         )
     @argument(
-        '-s', '--size', action='store', type=str, default='400,240',
+        '-s', '--size', action='store', type=str, default=None,
         help='Pixel size of plots, "width,height". Default is "--size 400,240".'
         )
     @argument(
@@ -210,11 +291,27 @@ class TikzMagics(Magics):
         help='Pgfplots libraries to load, separated by comma, e.g., -g fillbetween.'
         )
     @argument(
+        '-r', '--resolution', action='store', type=int, default=default_resolution,
+        help='Resolution of images. Default is {}dpi'.format(default_resolution)
+        )
+    @argument(
         '-S', '--save', action='store', type=str, default=None,
         help='Save a copy to file, e.g., -S filename. Default is None'
         )
     @argument('-i', '--imagemagick', action='store', type=str, default='convert',
         help='Name of ImageMagick executable, optionally with full path. Default is "convert"'
+        )
+    @argument('--dvisvgm', action='store', type=str, default='dvisvgm',
+        help='Name of dvi to svg executable, optionally with full path. Default is "dvisvgm"'
+        )
+    @argument('--ghostscript', action='store', type=str, default='gs',
+        help='Name of ghostscript executable, optionally with full path. Default is "gs"'
+        )
+    @argument('--pdftocairo', action='store', type=str, default='gs',
+        help='Name of pdftocairo executable (used for "poppler" method), optionally with full path. Default is "pdftocairo"'
+        )
+    @argument('--convert', action='store', type=str, default=default_convert,
+        help='Convert program, options are "ghostscript","imagemagick","poppler" or "svg". Default is "{}"'.format(default_convert)
         )
     @argument('-po', '--pictureoptions', action='store', type=str, default='',
         help='Additional arguments to pass to the \\tikzpicture command.'
@@ -276,15 +373,17 @@ class TikzMagics(Magics):
         args = parse_argstring(self.tikz, line)
         scale = args.scale
         size = args.size
-        width, height = size.split(',')
         plot_format = args.format
         encoding = args.encoding
         tikz_library = args.library.split(',')
         pgfplots_library = args.pgfplotslibrary.split(',')
         latex_package = args.package.split(',')
         imagemagick_path = args.imagemagick
+        ghostscript_path = args.ghostscript
         picture_options = args.pictureoptions
         tikz_options = args.tikzoptions
+        convert = args.convert
+        resolution = args.resolution
 
         # arguments 'code' in line are prepended to the cell lines
         if cell is None:
@@ -305,8 +404,8 @@ class TikzMagics(Magics):
 
         add_params = ""
 
-        if plot_format == 'png' or plot_format == 'jpg' or plot_format == 'jpeg':
-            add_params += "density=300,"
+        if (plot_format == 'png' or plot_format == 'jpg' or plot_format == 'jpeg'):
+            add_params += "density={},".format(resolution)
 
         # choose between CircuiTikZ and regular Tikz
         if args.circuitikz:
@@ -315,12 +414,19 @@ class TikzMagics(Magics):
         else:
             tikz_env = 'tikzpicture'
             tikz_package = 'tikz'
+        
+        texclass=[]
+        if convert=='imagemagick':
+            if size is not  None:
+                width, height = size.split(',')
+            else:
+                width = default_width
+                height = default_height
+            texclass.append('''\n\\documentclass[convert={convertexe={%(imagemagick_path)s},%(add_params)ssize=%(width)sx%(height)s,outext=.%(plot_format)s},border=0pt]{standalone}\n''' % locals())
+        else:
+            texclass.append('''\n\\documentclass[border=0pt]{standalone}\n''')
 
         tex = []
-        tex.append('''
-\\documentclass[convert={convertexe={%(imagemagick_path)s},%(add_params)ssize=%(width)sx%(height)s,outext=.png},border=0pt]{standalone}
-''' % locals())
-
         tex.append('\\usepackage[%(tikz_options)s]{%(tikz_package)s}\n' % locals())
 
         if latex_package != [""]:
@@ -338,48 +444,100 @@ class TikzMagics(Magics):
         if args.preamble is not None:
             tex.append('''%s\n''' % args.preamble)
 
-        tex.append('''\\begin{document}
-\\begin{%(tikz_env)s}[scale=%(scale)s,%(picture_options)s]
-''' % locals())
+        tex.append('''\\begin{document}\n\\begin{%(tikz_env)s}[scale=%(scale)s,%(picture_options)s]\n''' % locals())
 
         tex.append(code)
 
-        tex.append('''
-\\end{%(tikz_env)s}
-\\end{document}
-''' % locals())
+        tex.append('''\\end{%(tikz_env)s}\n\\end{document}\n''' % locals())
 
-        code = str('').join(tex)
+        code = str('').join(texclass+tex)
 
         if args.showlatex:
             print(code)
             return
+        
 
-        latex_log = self._run_latex(code, encoding, plot_dir)
+        print(plot_dir)
 
         key = 'TikZMagic.Tikz'
         display_data = []
 
         # If the latex error log exists, then image generation has failed.
         # Publish error log and return immediately
-        if latex_log:
-            self._publish_display_data(source=key, data={'text/plain': latex_log})
-            return
 
-        if plot_format == 'jpg' or plot_format == 'jpeg':
-            self._convert_png_to_jpg(plot_dir, imagemagick_path)
-        elif plot_format == 'svg':
-            self._convert_pdf_to_svg(plot_dir)
+        if convert!='imagemagick':
+            if plot_format=='jpg' or plot_format == 'jpeg' or plot_format == 'png':
+                if convert=='svg':
+                    latex_log = self._run_latex(code, encoding, plot_dir,exe="latex")
+                    self._shell(plot_dir,'ls')
+                    if latex_log:
+                        self._publish_display_data(source=key, data={'text/plain': latex_log})
+                        return
+                    self._convert_dvi_to_svg(plot_dir)
+                    if size is not None:
+                        width, height = [int(s) for s in size.split(',')]
+                        #pw,ph=self._fix_gnuplot_svg_file(plot_dir)
+                        pw,ph=self._get_size_from_svg(plot_dir)
+                        rw=(72*width)//pw
+                        rh=(72*height)//ph
+                        print(width,height,pw,ph,rw,rh)
+                        self._convert_svg_to_raster(plot_dir,imagemagick_path,rw,rh,width,height,plot_format=plot_format)
+                    else:
+                        self._convert_svg_to_raster(plot_dir,imagemagick_path,resolution,plot_format=plot_format)
+                elif convert == 'ghostscript':
+                    latex_log = self._run_latex(code, encoding, plot_dir,exe="pdflatex")
+                    if latex_log:
+                        self._publish_display_data(source=key, data={'text/plain': latex_log})
+                        return
+                    texclass=[]
+                    if size is not None:
+                        width, height = [int(s) for s in size.split(',')]
+                        pw,ph=self._get_size_from_svg(plot_dir)
+                        rw=72*width//pw
+                        rh=72*width//ph
+                        texclass.append('''\n\\documentclass[convert={{convertexe={{{ghostscript_path}}},gsdevice=pngalpha,density={rw}x{rh},outext=.{plot_format}}},border=0pt]{{standalone}}\n'''.format(**locals()))
+                    else:
+                        texclass.append('''\n\\documentclass[convert={{convertexe={{{ghostscript_path}}},gsdevice=pngalpha,density={resolution},outext=.{plot_format}}},border=0pt]{{standalone}}\n'''.format(**locals()))
+                    code = str('').join(texclass+tex)
+                    latex_log = self._run_latex(code, encoding, plot_dir,exe="latex")
+                    if latex_log:
+                        self._publish_display_data(source=key, data={'text/plain': latex_log})
+                        return
+                else: # convert == 'poppler'
+                    latex_log = self._run_latex(code, encoding, plot_dir,exe="pdflatex")
+                    if latex_log:
+                        self._publish_display_data(source=key, data={'text/plain': latex_log})
+                        return
+                    if size is not None:
+                        width, height = [int(s) for s in size.split(',')]
+                        self._run_pdftocairo(plot_dir,pdftocaito_path,width=width,height=height,plot_format=plot_format)
+                    else:
+                        self._run_pdftocairo(plot_dir,pdftocairo_path,resolution=resolution,plot_format=plot_format)
+        else:
+            latex_log = self._run_latex(code, encoding, plot_dir)
+            if latex_log:
+                self._publish_display_data(source=key, data={'text/plain': latex_log})
+                return
+            if plot_format == 'jpg' or plot_format == 'jpeg':
+                self._convert_png_to_jpg(plot_dir, imagemagick_path)
+            elif plot_format == 'svg':
+                self._convert_pdf_to_svg(plot_dir)
 
         image_filename = "%s/tikz.%s" % (plot_dir, plot_format)
+        
+        
 
         # Publish image
         try:
             image = open(image_filename, 'rb').read()
             plot_mime_type = _mimetypes.get(plot_format, 'image/%s' % (plot_format))
-            width, height = [int(s) for s in size.split(',')]
+            #if plot_format == 'svg' and convert=='imagemagick':
             if plot_format == 'svg':
-                image = self._fix_gnuplot_svg_size(image, size=(width, height))
+                if size is not None:
+                    size_spec = [int(s) for s in size.split(',')]
+                else:
+                    size_spec=None
+                image = self._fix_gnuplot_svg_size(image, size=size_spec)
             display_data.append((key, {plot_mime_type: image}))
 
         except IOError:
@@ -388,8 +546,9 @@ class TikzMagics(Magics):
         # Copy output file if requested
         if args.save is not None:
             copy(image_filename, args.save)
+        print(plot_dir)
 
-        rmtree(plot_dir)
+        #rmtree(plot_dir)
 
         for tag, disp_d in display_data:
             if plot_format == 'svg':
